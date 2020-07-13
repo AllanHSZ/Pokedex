1c23be995aabaddffa4ad4fa8f5713313b269ff7
Allan <allanhsz@outlook.com>
2020-07-13 20:31:52 -0300

    Removendo a bottomapp bar

--- app/src/main/java/com/allanhsz/pokedex/activities/MainActivity.java
+++ app/src/main/java/com/allanhsz/pokedex/activities/MainActivity.java
@@ -14,6 +14,7 @@ import com.allanhsz.pokedex.R;
 import com.allanhsz.pokedex.adapters.PokemonAdapter;
 import com.allanhsz.pokedex.model.Pokemon;
 import com.allanhsz.pokedex.utils.HandlerErro;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +25,12 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final ArrayList<Pokemon> pokemons = new ArrayList<>();
     private ImageView loading;
     private RecyclerView rvPokemon;
     private PokemonAdapter adapter;
+    private FloatingActionButton add;
+
+    private final ArrayList<Pokemon> pokemons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +38,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loading = findViewById(R.id.Loading);
+
         rvPokemon = findViewById(R.id.RvPokemon);
         rvPokemon.setLayoutManager(new GridLayoutManager(this, 1));
+        rvPokemon.post(new Runnable() {
+            @Override
+            public void run() {
+                rvPokemon.setPadding(0 , (int)getResources().getDimension(R.dimen.spaceMD),0,(int)(getResources().getDimension(R.dimen.spaceLG)*2+add.getHeight()));
+            }
+        });
+
 
         adapter = new PokemonAdapter(this, pokemons);
         rvPokemon.setAdapter(adapter);
 
-        findViewById(R.id.Add).setOnClickListener(new View.OnClickListener() {
+        add = findViewById(R.id.Add);
+
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, PokemonActivity.class));
